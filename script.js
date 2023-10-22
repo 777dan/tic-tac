6329ff9ccd7978b7043024467c58f5a5e65c9a95
777dan <danya.nikitin7@gmail.com>
2023-10-22 14:45:38 +0300

    added setTimeout and other functions

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 let tick = true;
-let counter = 0;
-let d = new Date();
-let dif = 0;
-function ticker() {
-    let d1 = new Date();
-    if (dif > 10) {
-        clearInterval(interval);
+let ticker = () => {
+    if (tick) {
+        document.write("Тик ");
+        tick = false;
     } else {
-        if (tick) {
-            document.write("Тик ");
-            tick = false;
-        } else {
-            document.write("Так ");
-            tick = true;
-        }
-        counter++;
-        dif = (d1.getTime() - d.getTime()) / 1000;
-        console.log(dif);
+        document.write("Так ");
+        tick = true;
     }
 }
-let interval = setInterval(ticker, 1000);
+
+let setTime = () => {
+    seconds = +prompt("Input a number of seconds");
+    return seconds * 1000;
+}
+
+let startTimer = (seconds) => {
+    let interval = setInterval(ticker, 1000);
+    setTimeout(() => clearInterval(interval), seconds);
+}
